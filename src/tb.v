13e955115e28c45b2e5b73c568a1ac2336a6585a
Toivo Henningsson <toivo.h.h@gmail.com>
2023-10-22 21:07:34 +0200

    Use new top module

--- src/tb.v
+++ src/tb.v
@@ -23,12 +23,11 @@ module tb ();
     reg  [7:0] ui_in;
     reg  [7:0] uio_in;
 
-    wire [6:0] segments = uo_out[6:0];
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_toivoh_test tt_um_seven_segment_seconds (
+    tt_um_toivoh_test dut (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
--- src/test.py
+++ src/test.py
@@ -3,46 +3,30 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
-async def test_7seg(dut):
-    dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+async def test(dut):
+	dut._log.info("start")
+	clock = Clock(dut.clk, 2, units="us")
+	cocotb.start_soon(clock.start())
 
-    # reset
-    dut._log.info("reset")
-    dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+	# reset
+	dut._log.info("reset")
+	dut.rst_n.value = 0
+	# set the compare value
+	await ClockCycles(dut.clk, 10)
+	dut.rst_n.value = 1
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+	# enable
+	dut.ena.value = 1
 
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
+	period = (512 + 56) << 3;
 
-    # reset
-    dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
+	with open("tb-data.txt", "w") as file:
+		file.write("data = [")
+		for i in range(2*period):
+			file.write(str(0 + dut.dut.oct_counter.value) + " ")
+			file.write(str(0 + dut.dut.saw_counter.counter.value) + " ")
+			file.write(str(0 + dut.dut.saw.value) + " ")
+			file.write(";")
+			await ClockCycles(dut.clk, 1)
+		file.write("]")
