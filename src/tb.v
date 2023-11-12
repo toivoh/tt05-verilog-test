d20170f2e7ea221945e27c015e2fda5affad935e
Toivo Henningsson <toivo.h.h@gmail.com>
2023-11-12 15:05:05 +0100

    Add serial RAM model to tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,10 +1,45 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
+// set RAM_ADDR_BITS <= ADDR_PINS*2**LOG2_CYCLES
+module serial_ram #( parameter ADDR_PINS=4, DATA_PINS=4, LOG2_CYCLES=2, RAM_ADDR_BITS=12, DELAY=1) (
+        input wire clk,
+        input wire reset,
+        input wire enable,
+
+        input wire  [ADDR_PINS-1:0] addr_in,
+        output wire [DATA_PINS-1:0] data_out
+    );
+
+    localparam CYCLES = 2**LOG2_CYCLES;
+
+    localparam ADDR_BITS = ADDR_PINS*CYCLES;
+    localparam DATA_BITS = DATA_PINS*CYCLES;
+
+    reg [LOG2_CYCLES-1:0] counter;
+    reg [ADDR_BITS-1:0] addr;
+    reg [DATA_BITS-1:0] data;
+
+    reg [DATA_BITS-1:0] ram[2**RAM_ADDR_BITS];
+
+    reg [DATA_PINS*DELAY-1:0] sr;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            counter <= 0;
+        end else if (enable) begin
+            addr[ADDR_PINS*counter + ADDR_PINS-1 -: ADDR_PINS] <= addr_in;
+            if (counter == 0) data <= ram[addr[RAM_ADDR_BITS-1:0]];
+            else data <= data >> DATA_PINS;
+            counter <= counter + 1;
+
+            sr <= {data[DATA_PINS-1:0], sr[DATA_PINS*DELAY-1:DATA_PINS]};
+        end
+    end
+
+    assign data_out = sr[DATA_PINS-1:0];
+endmodule
+
 
 // testbench is controlled by test.py
 module tb ();
