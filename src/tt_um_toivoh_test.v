5c8270b49e084e264f81a7c41ce4f378d3990af1
Toivo Henningsson <toivo.h.h@gmail.com>
2023-11-12 09:52:19 +0100

    Test simple VGA raster scan with adjustable VGA timings

--- src/tt_um_toivoh_test.v
+++ src/tt_um_toivoh_test.v
@@ -1,6 +1,44 @@
 `default_nettype none
 
-module tt_um_toivoh_test #( parameter LOG2_BYTES_IN = 3, parameter LOG2_BYTES_OUT = 2) (
+module raster_scan #( parameter X_BITS=11, Y_BITS=10 ) (
+		input wire clk,
+		input wire reset,
+
+		// Assume visible area starts at zero
+		input wire signed [X_BITS-1:0] x0, x_fp, x_s, x1,
+		input wire signed [Y_BITS-1:0] y0, y_fp, y_s, y1,
+
+		output wire active, hsync, vsync
+	);
+
+	reg signed [X_BITS-1:0] x;
+	reg signed [Y_BITS-1:0] y;
+
+	wire last_x = (x == x1);
+	wire signed [X_BITS-1:0] next_x = last_x ? x0 : x + 1;
+	wire last_y = (y == y1);
+	wire signed [Y_BITS-1:0] next_y = last_y ? y0 : y + 1;
+
+	wire x_active = (x >= 0) && (x < x_fp);
+	wire y_active = (y >= 0) && (y < y_fp);
+	assign active = x_active && y_active;
+
+	assign hsync = x >= x_s;
+	assign vsync = y >= y_s;
+
+	always @(posedge clk) begin
+		if (reset) begin
+			x <= 0;
+			y <= 0;
+		end else begin
+			x <= next_x;
+			if (last_x) y <= next_y;
+		end
+	end
+endmodule
+
+
+module tt_um_toivoh_test #( parameter LOG2_BYTES_IN = 4, X_BITS=11, Y_BITS=10 ) (
 		input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
 		output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
 		input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -12,54 +50,35 @@ module tt_um_toivoh_test #( parameter LOG2_BYTES_IN = 3, parameter LOG2_BYTES_OU
 	);
 
 	localparam BYTES_IN = 1 << LOG2_BYTES_IN;
-	localparam BYTES_OUT = 1 << LOG2_BYTES_OUT;
 
-	localparam BITS_OUT = 8*BYTES_OUT;
+	wire reset = !rst_n;
 
-	assign uio_out = 0;
 	assign uio_oe = 0;
 
-	reg  [BYTES_IN*8-1:0]  input_data;
-	wire [BYTES_OUT*8-1:0] result;
-	reg  [BYTES_OUT*8-1:0] output_data;
-
+	reg  [BYTES_IN*8-1:0] cfg;
 	wire [7:0] data_in = ui_in;
-	wire [LOG2_BYTES_IN-1:0]  sel_in  = uio_in[LOG2_BYTES_IN-1:0];
-	wire [LOG2_BYTES_OUT-1:0] sel_out = uio_in[4+LOG2_BYTES_OUT-1:4];
-	assign uo_out = output_data[7+sel_out*8 -: 8];
+	wire [LOG2_BYTES_IN-1:0] sel_in = uio_in[LOG2_BYTES_IN-1:0];
 
-	wire [BYTES_IN*4-1:0] x = input_data[BYTES_IN*4-1:0];
-	wire [BYTES_IN*4-1:0] y = input_data[BYTES_IN*8-1:BYTES_IN*4];
-
-	//assign result = !(x&y); // NAND
-	//assign result = x + y; // add
-	//assign result = $signed(x) >>> y[4:0]; // barrel shifter
-	//assign result = !(x&y[4:0]); // NAND / not for barrel shifter comparison
-	//assign result = ena ? x : y; // mux
-		//assign result = rst_n ? (ena ? x : y) : (ena ? {x[0], x[BYTES_IN*4-1:1]} : {y[0], y[BYTES_IN*4-1:1]}); // mux4
-
-	// Ripple carry adder
-	wire [BITS_OUT:0] c;
-	assign c[0] = 0;
-
-	wire [2:0] temp[BITS_OUT];
-	genvar i;
-	generate
-		for (i=0; i < BITS_OUT; i++) begin
-			//assign {c[i+1], result[i]} = x[i] + y[i] + c[i];
-			//assign c[i+1] = x[i]&y[i] | c[i]&(x[i] | y[i]);
-			//assign result[i] = c[i]&x[i]&y[i] | (c[i] | x[i] | y[i])&~c[i+1];
-
-			assign temp[i] = {x[i], 1'b1} + {y[i], c[i]};
-			assign {c[i+1], result[i]} = temp[i][2:1];
-		end
-	endgenerate
-
-	always @(posedge clk) begin : main
+	always @(posedge clk) begin
 		integer i;
 		for (i = 0; i < BYTES_IN; i = i + 1) begin
-			if (sel_in == i) input_data[i*8+7 -: 8] <= data_in;
+			if (sel_in == i) cfg[i*8+7 -: 8] <= data_in;
 		end
-		output_data <= result;
 	end
+
+	wire [X_BITS-1:0] x0, x_fp, x_s, x1;
+	wire [Y_BITS-1:0] y0, y_fp, y_s, y1;
+	assign {x1, x_s, x_fp, x0} = cfg[X_BITS*4-1:0];
+	assign {y1, y_s, y_fp, y0} = cfg[(X_BITS+Y_BITS)*4-1:X_BITS*4];
+
+	wire active, hsync, vsync;
+	raster_scan #(.X_BITS(X_BITS), .Y_BITS(Y_BITS)) rs(
+		.clk(clk), .reset(reset),
+		.x0(x0), .x_fp(x_fp), .x_s(x_s), .x1(x1),
+		.y0(y0), .y_fp(y_fp), .y_s(y_s), .y1(y1),
+
+		.active(active), .hsync(hsync), .vsync(vsync)
+	);
+
+	assign uio_out = {5'b0, vsync, hsync, active};
 endmodule
